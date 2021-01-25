09e17f21d3aa3fcbee5672b170a424a8f71d5073
Hylson <hvescovi@gmail.com>
2021-01-25 14:02:36 -0300

    piscar leds

---
+++ 2021/02-piscar-leds/02-piscar-leds.ino
@@ -0,0 +1,27 @@
+void setup() { // código executado somente uma vez
+
+
+  // configurar pinos de saída (leds)
+  for (int i = 2; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+  }
+  
+}
+
+void loop() { // código executado repetidamente
+
+  // acender os leds
+  for (int i = 2; i <= 13; i++) {
+    digitalWrite(i, HIGH);
+  }
+  
+  delay(1000); // esperar 1 segundo
+  
+  // apagar os leds
+  for (int i = 2; i <= 13; i++) {
+    digitalWrite(i, LOW);
+  }
+
+  delay(1000); // esperar 1 segundo
+  
+}
